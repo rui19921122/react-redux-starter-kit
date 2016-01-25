0350d3c8358ace6e902efac95d9a71edba9dc0fd
rui19921122 <rui19921122@qq.com>
2016-01-25 21:44:12 +0800

    update package.json

--- src/main.js
+++ src/main.js
@@ -6,14 +6,14 @@ import routes from './routes'
 import Root from './containers/Root'
 import configureStore from './redux/configureStore'
 
-const historyConfig = { basename: __BASENAME__ }
+const historyConfig = {basename: __BASENAME__}
 const history = useRouterHistory(createHistory)(historyConfig)
 
 const initialState = window.__INITIAL_STATE__
-const store = configureStore({ initialState, history })
+const store = configureStore({initialState, history})
 
 // Render the React application to the DOM
 ReactDOM.render(
-  <Root history={history} routes={routes} store={store} />,
+  <Root history={history} routes={routes} store={store}/>,
   document.getElementById('root')
 )
