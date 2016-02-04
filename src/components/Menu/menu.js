8884759daf30c8df71249976c98b020d984c845e
hanrui <rui19921122@qq.com>
2016-02-04 21:49:54 +0800

    add ant.design

--- src/components/Menu/menu.js
+++ src/components/Menu/menu.js
@@ -5,47 +5,47 @@
 import {Menu, Icon} from 'antd'
 import React from 'react'
 import {Link} from 'react-router'
-import './menu.css'
 export default class CustomMenu extends React.Component {
-  //todo 解决链接第一次点击不跳转问题
-
-  static processSingle(single, index) {
-    return <Menu.Item key={single.key}><Link to={single.href}>{single.name}</Link></Menu.Item>
-  };
-
-  static processMultiChildren(single, index) {
-    return <Menu.Item key={single.key}>
-      <Link to={single.href}>{single.name}</Link>
-    </Menu.Item>
-  };
-
-  static processMulti(multi, index) {
-    return (<Menu.SubMenu title={multi.name} key={index+1}>
-      {multi.children.map(CustomMenu.processMultiChildren)}
-    </Menu.SubMenu>);
-  };
-
-  static process(inner, index) {
-    if (inner.type === 'single') {
-      return CustomMenu.processSingle(inner, index)
-    } else {
-      return CustomMenu.processMulti(inner, index)
-    }
-  }
+	//todo 解决链接第一次点击不跳转问题
 
 
-  render() {
-    return (
-      <Menu
-        mode="inline"
-        defaultSelectedKeys={[this.props._menu]}
-      >
-        {this.props.menu.map(CustomMenu.process)}
-      </Menu>
-    )
-  }
+	static processSingle(single, index) {
+		return <Menu.Item key={single.key}><Link to={single.href}>{single.name}</Link></Menu.Item>
+	};
+
+	static processMultiChildren(single, index) {
+		return <Menu.Item key={single.key}>
+			<Link to={single.href}>{single.name}</Link>
+		</Menu.Item>
+	};
+
+	static processMulti(multi, index) {
+		return (<Menu.SubMenu title={multi.name} key={index+1}>
+			{multi.children.map(CustomMenu.processMultiChildren)}
+		</Menu.SubMenu>);
+	};
+
+	static process(inner, index) {
+		if (inner.type === 'single') {
+			return CustomMenu.processSingle(inner, index)
+		} else {
+			return CustomMenu.processMulti(inner, index)
+		}
+	}
+
+
+	render() {
+		return (
+			<Menu
+				mode="inline"
+				defaultSelectedKeys={[this.props._menu]}
+			>
+				{this.props.menu.map(CustomMenu.process)}
+			</Menu>
+		)
+	}
 }
 CustomMenu.propTypes = {
-  menu: React.PropTypes.array.isRequired,
-  _menu: React.PropTypes.string
+	menu: React.PropTypes.array.isRequired,
+	_menu: React.PropTypes.string
 };
--- src/redux/modules/menu.js
+++ src/redux/modules/menu.js
@@ -4,42 +4,44 @@
 import {createAction,handleActions} from 'redux-actions'
 
 export const MenuList = 'MenuList';
+export const UpdateMenu = 'UpdateMenu';
 
 const defaultMenu = [
-  {type: 'single', name: '首页', href: 'loginIn', key: 'index'},
-  {
-    type: 'multiple', name: '量化要求模块', href: '#', key: 'value0',
-    children: [
-      {type: 'single', name: '计划编制', href: 'makePlan', key: 'value1'},
-      {type: 'single', name: '计划查询', href: 'queryPlan', key: 'value2'},
-      {type: 'single', name: '计划审核', href: 'examPlan', key: 'value3'}
-    ]
-  },
-  {
-    type: 'multiple', name: '工作职责模块', href: '#', key: 'work0',
-    children: [
-      {type: 'single', name: '查看工作标准', href: 'queryWork', key: 'work1'},
-      {type: 'single', name: '录入实际工作', href: 'typeWork', key: 'work2'}
-    ]
-  },
-  {
-    type: 'multiple', name: '安全风险管控模块', href: '#', key: 'risk0',
-    children: [
-      {type: 'single', name: '发布风险', href: 'releaseRisk', key: 'risk1'},
-      {type: 'single', name: '编制检查计划', href: 'makeRisk', key: 'risk2'},
-      {type: 'single', name: '审批风险检查计划', href: 'examRisk', key: 'risk3'}
-    ]
-  }
+	{type: 'single', name: '首页', href: 'loginIn', key: 'index'},
+	{
+		type: 'multiple', name: '量化要求模块', href: '#', key: 'value0',
+		children: [
+			{type: 'single', name: '计划编制', href: 'makePlan', key: 'value1'},
+			{type: 'single', name: '计划查询', href: 'queryPlan', key: 'value2'},
+			{type: 'single', name: '计划审核', href: 'examPlan', key: 'value3'}
+		]
+	},
+	{
+		type: 'multiple', name: '工作职责模块', href: '#', key: 'work0',
+		children: [
+			{type: 'single', name: '查看工作标准', href: 'queryWork', key: 'work1'},
+			{type: 'single', name: '录入实际工作', href: 'typeWork', key: 'work2'}
+		]
+	},
+	{
+		type: 'multiple', name: '安全风险管控模块', href: '#', key: 'risk0',
+		children: [
+			{type: 'single', name: '发布风险', href: 'releaseRisk', key: 'risk1'},
+			{type: 'single', name: '编制检查计划', href: 'makeRisk', key: 'risk2'},
+			{type: 'single', name: '审批风险检查计划', href: 'examRisk', key: 'risk3'}
+		]
+	}
 ];
+export let updateMenu = createAction(UpdateMenu);
 
 export let getMenu = createAction(MenuList);
 //export default handleActions({
 //	[COUNTER_INCREMENT]: (state, { payload }) => state + payload
 //}, [1, 2])
 export default handleActions(
-  {
-    [MenuList]: (i)=>i
-  }
-  ,
-  defaultMenu
+	{
+		[MenuList]: (i)=>i
+	}
+	,
+	defaultMenu
 )
